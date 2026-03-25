d3dd0d89ebfe2e6721474b6ae9d9efccee350903
124049927-lang <124049927@upq.edu.mx>
2026-03-25 11:56:41 -0600

    SqlAquemy & PostgreSQL parte 2

--- miAPI/app/data/db.py
+++ miAPI/app/data/db.py
@@ -5,14 +5,25 @@ import os
 #definimos la URL de conexión a la base de datos
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:123456@localhost:5434/DB_miapi"
+    "postgresql://postgres:123456@localhost:5432/DB_miapi"
 )
 
 #creamos el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
 #creamos una sesión para interactuar con la base de datos
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine)
 
 #creamos la base para los modelos
 Base = declarative_base()
+
+#Función par ala sesión en cada petición
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- miAPI/app/models/usuario.py
+++ miAPI/app/models/usuario.py
@@ -2,6 +2,5 @@ from pydantic import BaseModel, Field
 
 #Modelo de validación Pydantic
 class usuario_create(BaseModel):
-    id: int = Field(..., gt=0, description="Identificador")
     nombre: str = Field(..., min_length=3, max_length=50, example="Rogelio")
     edad: int = Field(..., ge=1, le=123, description="Edad valida entre 1 y 123")
